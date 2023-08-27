6520138ba7ed016857421ba4d48f85d6f3d86e4d
Srinivasulu2693 <108609768+Srinivasulu2693@users.noreply.github.com>
2023-08-27 16:42:41 +0530

    Update aprioror.R

--- aprioror.R
+++ aprioror.R
@@ -1,7 +1,26 @@
-original_vector <- c(3,5,5,8,9,12,12,13,15,16,17,19,22,24,25,134)
-x <-mean(original_vector)
-print(paste("Mean:",x))
-u <-sd(original_vector)
-print(paste("S.D:",u))
-normalized_vector <- (original_vector - x) / u
-print(normalized_vector)
+#Load libraries
+library(arules)
+library(arulesViz)
+
+#Load data as transactions
+data=read.transactions("file:///D:/Data Sets - R/Store Data Apyori/store data.csv")
+inspect(head(data))
+list(data) #7501 transactions
+
+#Find top 10 most frequent items
+frequentItems=eclat(data,parameter=list(supp=0.07,maxlen=15))
+inspect(frequentItems)
+
+#Plot frequency of items
+itemFrequencyPlot(data,topN=10,main="Item Frquency")
+
+#Apriori algorithm
+rules=apriori(data,parameter=list(supp=0.001, confidence=0.5))
+
+#Top 10 rules by confidence
+sort_rules=sort(rules,by="confidence",decreasing=TRUE)
+inspect(head(sort_rules,n=10))
+
+#Visualize association rules
+subrules=head(rules,n=10,by="confidence")
+plot(subrules,method="graph")
